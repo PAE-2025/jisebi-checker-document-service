1a7c2518f30b54c1430e123704b18a0757b7b53d
Thaofoo <mthafixh@gmail.com>
2025-05-11 17:17:14 +0700

    add: implement google storage

---
+++ src/storage.py
@@ -0,0 +1,46 @@
+from google.cloud import storage
+from src.core.config import get_settings
+import os
+import io
+
+credentials_path = get_settings().GCS_CREDENTIALS_FILE
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+
+class GoogleStorage:
+
+    def __init__(self, bucket_name):
+        self.storage_client   = storage.Client()
+        self.bucket_name      = bucket_name
+        self.bucket           = self.ensure_bucket_exists()
+
+    # Make sure the bucket exists
+    def ensure_bucket_exists(self):
+        try:
+            bucket = self.storage_client.get_bucket(self.bucket_name)
+        except Exception:
+            bucket = self.storage_client.create_bucket(self.bucket_name)
+        return bucket
+
+    # Upload a file to a specific folder in the bucket
+    def upload_to_gcp(self, file_content, folder_name: str, file_name: str) -> str:
+        """Upload a file to a specific folder in the GCP bucket.
+        
+        Supports both `bytes` and `IO[bytes]` file content.
+        """
+        blob_path = f"{folder_name}/{file_name}"
+        blob = self.bucket.blob(blob_path)
+
+        if isinstance(file_content, io.IOBase):
+            file_content.seek(0)
+            blob.upload_from_file(file_content)
+        else:
+            raise TypeError("file_content must be an IO[bytes] object")
+        
+        return blob_path
+
+    # Delete a file from the bucket
+    def delete_from_gcp(self, file_path):
+        blob = self.bucket.blob(file_path)
+        blob.delete()
+
+storage = GoogleStorage(get_settings().GCS_BUCKET_NAME)

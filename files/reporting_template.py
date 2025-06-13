8bd8a1eefa52384c828ccc8e5b62dbd2c999e79b
Thaofoo <mthafixh@gmail.com>
2025-06-13 07:12:42 +0700

    adjust: remove raw json on the reporting

--- files/reporting_template.py
+++ files/reporting_template.py
@@ -295,14 +295,6 @@ template_str = """
     {% endfor %}
     </div>
 
-    <div>
-        {{ data["novelty"] }}
-    </div>
-
-    <div>
-        {{ data["semantic"] }}
-    </div>
-
     <script>
         // Calculate actual statistics
         document.addEventListener('DOMContentLoaded', function() {
